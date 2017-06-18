96ce004f1f3895b5914bdcab846f92cca706f7ad
tushgup <tush1998@gmail.com>
2017-06-18 22:42:47 +0530

    Added page token, verify token.

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var conf = module.exports = {
     FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
-    VERIFY_TOKEN: 'verify token here',
-    PROFILE_TOKEN: 'page access token here',
+    VERIFY_TOKEN: 'shoppingo_the_app',
+    PROFILE_TOKEN: 'EAAHBP2MrrpMBAGghUiUdAzk7GMbOtJVhCy8HpLeLtMRCtvEMBPWcKGGmBlQ2ienEGbiPe2mgxcZAuT3t1wICSOAstyqnb87vaGDak2dV5qyUf3IDLz8Ta0YwjtHx5DrLmdVSVRILXuyIkLGkkGjjefo2yDLrQ8kAjAuOFVAZDZD',
     PORT: '3000'
 };
