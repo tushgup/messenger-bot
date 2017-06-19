2d28d136fc5737f836c9738118c91e5b8e432b0a
tushgup <tush1998@gmail.com>
2017-06-19 13:34:38 +0530

    Added echo functionality v1

--- server.js
+++ server.js
@@ -52,37 +52,109 @@ app.get('/webhook', function(req, res) {
     }
 });
 
-function sendMessage(receiver, data, isText) {
-    var payload = {};
-    payload = data;
+app.post('/webhook', function (req, res) {
+    var data = req.body;
 
-    if(isText) {
-        payload = {
-            text: data
-        };
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function(entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
+
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.message) {
+                    receivedMessage(event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
     }
+});
 
-    request({
-        url: conf.FB_MESSAGE_URL,
-        method: 'POST',
-        qs: {
-            access_token: process.env.PROFILE_TOKEN || conf.PROFILE_TOKEN
-        },
-        json: {
-            recipient: {id: receiver},
-            message: payload
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:",
+        senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+
+        // If we receive a text message, check to see if it matches a keyword
+        // and send back the example. Otherwise, just echo the text we received.
+        switch (messageText) {
+            case 'generic':
+                sendGenericMessage(senderID);
+                break;
+
+            default:
+                sendTextMessage(senderID, messageText);
         }
-    }, function (error, response) {
-        if(error) console.log('Error sending message: ', error);
-        if(response.body.error) console.log('Error: ', response.body.error);
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
+    }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+    // To be expanded in later sections
+}
+
+function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: messageText
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: conf.PROFILE_TOKEN },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s",
+                messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
     });
 }
+
 
 var port = process.env.PORT || 8000;
 httpServer.listen(port, function () {
     console.log("Express http server listening on port " + port);
 });
-
-// httpServer.listen(port, function () {
-//     console.log('Express server listening - %d, in %s mode', port, app.get('env'));
-// });
